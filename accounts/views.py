1d9e83b74364bc591090b50296b3ccf294a0db2d
goldendevuz <goldendevuz@gmail.com>
2025-04-12 14:34:43 +0500

    14.34.12.04.2025

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions, status
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import UserRegisterSerializer, UserProfileSerializer, CustomTokenObtainPairSerializer
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
@@ -16,19 +18,34 @@ class ProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
-class LogoutView(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+# class LogoutView(generics.GenericAPIView):
+#     permission_classes = [permissions.IsAuthenticated]
+#
+#     def post(self, request):
+#         try:
+#             request.user.auth_token.delete()
+#             return Response(
+#                 {'success': 'Foydalanuvchi muvaffaqqiyatli chiqdi'},
+#                 status=status.HTTP_200_OK,
+#             )
+#         except Exception as e:
+#             return Response(
+#                 {'error': str(e)},
+#                 status=status.HTTP_400_BAD_REQUEST,
+#             )
 
+class LogoutView(APIView):
     def post(self, request):
         try:
-            request.user.auth_token.delete()
+            refresh_token = request.data.get("refresh")
+            token = RefreshToken(refresh_token)
+            token.blacklist()
             return Response(
-                {'success': 'Foydalanuvchi muvaffaqqiyatli chiqdi'},
-                status=status.HTTP_200_OK,
+                {"detail": "Muvaffaqiyatli chiqish amalga oshirildi"},
+                status=status.HTTP_200_OK
             )
         except Exception as e:
             return Response(
-                {'error': str(e)},
-                status=status.HTTP_400_BAD_REQUEST,
+                {"detail": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
             )
-
--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-
 from .views import (
     CourseListCreateView,
     CourseDetailView,
     LessonListCreateView,
-    EnrollCourseView
+    EnrollCourseView,
+    IndividualTaskListCreateView,
+    IndividualTaskRetrieveUpdateDestroyView
 )
 
 urlpatterns = [
@@ -13,4 +14,7 @@ urlpatterns = [
     path('<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
     path('<int:pk>/enroll/', EnrollCourseView.as_view(), name='course-enroll'),
     path('<int:course_id>/lessons/', LessonListCreateView.as_view(), name='lesson-list'),
+    path('individual-tasks/', IndividualTaskListCreateView.as_view(), name='individual-task-list'),
+    path('individual-tasks/<int:pk>/', IndividualTaskRetrieveUpdateDestroyView.as_view(),
+         name='individual-task-detail'),
 ]
