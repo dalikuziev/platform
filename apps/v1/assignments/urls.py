01e19a184af3564cfeffebe0430af9126f5e8607
dalikuziev <dalikuziev@gmail.com>
2025-04-28 17:39:19 +0500

    2025.04.28/17:38

--- apps/v1/assignments/urls.py
+++ apps/v1/assignments/urls.py
@@ -7,7 +7,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('',
+    path('lessons/<int:lesson_id>/assignments/',
          AssignmentListCreateView.as_view(), name='assignment-list'),
     path('<int:assignment_id>/submit/',
          SubmissionCreateView.as_view(), name='submission-create'),
